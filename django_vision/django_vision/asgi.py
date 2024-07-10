1a4adbdce505a889e8b5959af1a6fb6bc78d97ba
Kostek <ksytnik73kwer@gmail.com>
2024-07-10 20:35:12 +0200

    Refactor ASGI config to use websocket_urlpatterns for websocket

--- django_vision/django_vision/asgi.py
+++ django_vision/django_vision/asgi.py
@@ -2,20 +2,13 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from django.urls import path
-from detection_app.consumers import ObjectDetectionConsumer
+from .routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_vision.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    path("ws/detect/", ObjectDetectionConsumer.as_asgi()),
-                ]
-            )
-        ),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
